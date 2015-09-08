99715cc8a2be0488400245a8869d3e037d11f9f1
Tommy-Geenexus <roboter972@gmail.com>
2015-09-08 21:12:39 +0200

    kgsl: make trustzone default, replace conservative mutex with spinlock * Heating is greatly decreased with 5.1.1, thus tz can be default again for performance reasons.

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -49,7 +49,13 @@
 #define KGSL_NOP_IB_IDENTIFIER	        0x20F20F20
 #define KGSL_NOP_DATA_FILLER		0xFEEDFACE
 
+#ifdef CONFIG_MSM_SCM
+#define ADRENO_DEFAULT_PWRSCALE_POLICY  (&kgsl_pwrscale_policy_tz)
+#elif defined CONFIG_MSM_SLEEP_STATS_DEVICE
+#define ADRENO_DEFAULT_PWRSCALE_POLICY  (&kgsl_pwrscale_policy_idlestats)
+#else
 #define ADRENO_DEFAULT_PWRSCALE_POLICY  (&kgsl_pwrscale_policy_conservative)
+#endif
 
 #define ADRENO_ISTORE_START 0x5000 /* Istore offset */
 
